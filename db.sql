8efec29427e2ca0f2253de2c5c52257b82a9be0c
Moses Moletsane <mosesmoletsane17@gmail.com>
2023-08-13 16:58:42 +0200

    sql

--- db.sql
+++ db.sql
@@ -1,21 +1,10 @@
-CREATE TABLE price_plan (
+CREATE TABLE if not EXISTS price_plan (
     id integer primary key AUTOINCREMENT,
     plan_name text,
     sms_price real,
     call_price real
 );
-
-
 SELECT *
 FROM price_plan;
 SELECT count(*)
 FROM price_plan;
-
--- UPDATE price_plan SET total = sms_price + call_price;
-
-insert into price_plan (plan_name, sms_price, call_price)
-values ('sms 101', 2.35, 0.37);
-insert into price_plan (plan_name, sms_price, call_price)
-values ('call 101', 1.75, 0.65);
-insert into price_plan (plan_name, sms_price, call_price)
-values ('call 201', 1.85, 0.85);
--- migrations/001-create-price_plan.sql
+++ migrations/001-create-price_plan.sql
@@ -1,4 +1,4 @@
-CREATE TABLE price_plan (
+CREATE TABLE if not EXISTS price_plan (
     id integer primary key AUTOINCREMENT,
     plan_name text,
     sms_price real,
